83bc2d2fd03296e5a8849632959b5f258bab10fa
Alec Ortega <aleccortega@gmail.com>
2018-10-31 10:39:33 -0400

    Remove alias

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,6 @@ module.exports = {
   resolve: {
     alias: {
       react: path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
     },
   },
   externals: {
