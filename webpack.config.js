9a55c4d5b848128a41ec156e7f82f1994142762a
Alec Ortega <aleccortega@gmail.com>
2018-10-31 10:35:30 -0400

    Remove from externals

--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,48 @@ const package = require('./package.json');
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, "./src/index.js"),
+  entry: path.join(__dirname, './src/index.js'),
   output: {
     path: path.join(__dirname, './dist'),
     filename: 'index.js',
     library: package.name,
     libraryTarget: 'umd',
     publicPath: '/dist/',
-    umdNamedDefine: true
+    umdNamedDefine: true,
   },
   node: {
     net: 'empty',
     tls: 'empty',
-    dns: 'empty'
+    dns: 'empty',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: ["babel-loader"],
-        include: path.resolve(__dirname, "src"),
+        use: ['babel-loader'],
+        include: path.resolve(__dirname, 'src'),
         exclude: /node_modules/,
-      }
-    ]
+      },
+    ],
   },
   resolve: {
     alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),  
-    }
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
   },
   externals: {
     react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "React",
-      root: "React"
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React',
     },
-    "react-dom": {
-      commonjs: "react-dom",
-      commonjs2: "react-dom",
-      amd: "ReactDOM",
-      root: "ReactDOM"
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM',
     },
-    "i18next": "i18next",
-    "react-i18next": "react-i18next"
-  }
+  },
 };
