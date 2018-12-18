24fb3b4f2052c0469f7cf52e6fc55cd3d1ae7f31
Brandon Shar <brandon.shar@ezcater.com>
2018-12-18 15:45:26 -0500

    add export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 export { default as configureI18n } from './utilities/configureI18n';
 export { default as FormattedMessage } from './components/FormattedMessage';
+export {
+  default as FormattedDictionary,
+} from './components/FormattedDictionary';
 export { default as FormattedDate } from './components/FormattedDate';
 export { default as FormattedTime } from './components/FormattedTime';
 export {
