56850197bbd6691d0dbc59f72a2a8ba057f28b37
Brandon Shar <brandon.shar@ezcater.com>
2018-12-20 10:25:57 -0500

    Only translate things that could be a translation

--- src/components/__tests__/FormattedDictionary.test.js
+++ src/components/__tests__/FormattedDictionary.test.js
@@ -20,6 +20,27 @@ describe('<FormattedDictionary />', () => {
       expect(wrapper.contains(<p>{MOCKED_RETURN_VALUE}</p>)).toEqual(true);
     });
 
+    it("doesn't include non-string params that may be provided by wrapping components", () => {
+      const MOCKED_RETURN_VALUE =
+        'key: name.key options: {} - key: header.key options: {}';
+      const wrapper = mount(
+        <FormattedDictionary
+          name="name.key"
+          header="header.key"
+          something={{ bad: 'value' }}
+        >
+          {({ name, header, something }) => (
+            <p>
+              {name} - {header}
+              {something}
+            </p>
+          )}
+        </FormattedDictionary>
+      );
+
+      expect(wrapper.contains(<p>{MOCKED_RETURN_VALUE}</p>)).toEqual(true);
+    });
+
     it('handles options being passed', () => {
       const MOCKED_RETURN_VALUE = `key: name.key options: {} - key: header.key options: ${JSON.stringify(
         { num: 4 }
--- src/formatters/formatDictionary.js
+++ src/formatters/formatDictionary.js
@@ -1,10 +1,13 @@
 import formatMessage from './formatMessage';
 
+const looksLikeATranslation = id => typeof id === 'string';
+
 export default (t, ids, options = {}) => {
   const translations = {};
-  Object.keys(ids).forEach(
-    key => (translations[key] = formatMessage(t, ids[key], options[key]))
-  );
+  Object.keys(ids).forEach(key => {
+    if (looksLikeATranslation(ids[key]))
+      translations[key] = formatMessage(t, ids[key], options[key]);
+  });
 
   return translations;
 };
